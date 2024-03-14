72c04fa7d936f90dfe3ee6427f4daf827d3b22ee
Rudresh Pandey <pandeyrudresh203@gmail.com>
2024-03-14 11:05:51 +0530

    Auth Setup

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import NavBar from './components/NavBar'
-import Landing from './views/Landing'
+import React from "react";
+import NavBar from "./components/NavBar";
+import Landing from "./views/Landing";
+import Register from "./views/Register";
+import Login from "./views/Login";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 const App = () => {
   return (
-    <>
-    <NavBar/>
-    <Landing/>
-    </>
-  )
-}
+    <Router>
+      <NavBar />
 
-export default App
+      <Routes>
+        <Route path="/signUp" element={<Register />} />
+      </Routes>
+      <Routes>
+        <Route path='/signin' element={<Login />} />
+      </Routes>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
